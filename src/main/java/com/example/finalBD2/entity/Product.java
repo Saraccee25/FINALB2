cc5b2058f53963b52af08d79295459fe9031690f
IsaMontoya17 <isabelamontoya2021@gmail.com>
2024-12-10 17:49:35 -0500

    Feat(backend): Add validations to all attributes

--- src/main/java/com/example/finalBD2/entity/Product.java
+++ src/main/java/com/example/finalBD2/entity/Product.java
@@ -1,5 +1,9 @@
 package com.example.finalBD2.entity;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,11 +14,21 @@ public class Product {
 
     @Id
     private String id;
+
+    @NotBlank(message = "El nombre del producto no puede estar vacío.")
+    @Size(min = 2, max = 100, message = "El nombre del producto debe tener entre 2 y 100 caracteres.")
     private String name;
+
+    @NotBlank(message = "La categoría no puede estar vacía.")
     private String category;
+
+    @DecimalMin(value = "0.01", message = "El precio debe ser mayor a 0.")
     private double price;
+
+    @Min(value = 0, message = "El stock no puede ser negativo.")
     private double stock;
+
+    @NotBlank(message = "El tamaño del producto no puede estar vacío.")
     private String size;
 
 }
-
